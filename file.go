0a4c1ffbd2adffc718eea0545a251fdb8baf7229
Joerg Fischer <raumstation@mail.ru>
2025-02-07 15:23:50 +0100

    correct os independent file handling

--- file.go
+++ file.go
@@ -4,39 +4,32 @@ import (
 	"context"
 	"golang.org/x/net/webdav"
 	"os"
-	"path/filepath"
 )
 
 type twdsFS struct {
-	base_path string
+	base_path webdav.Dir
 }
 
 func (fs *twdsFS) init(base_path string) {
-	fs.base_path = base_path
+	fs.base_path = webdav.Dir(base_path)
 }
 
 func (fs *twdsFS) Mkdir(ctx context.Context, name string, perm os.FileMode) error {
-	real_file_name := filepath.Join(fs.base_path, name)
-	return os.Mkdir(real_file_name, perm)
+	return fs.base_path.Mkdir(ctx, name, perm)
 }
 
 func (fs *twdsFS) OpenFile(ctx context.Context, name string, flag int, perm os.FileMode) (webdav.File, error) {
-	real_file_name := filepath.Join(fs.base_path, name)
-	return os.OpenFile(real_file_name, flag, perm)
+	return fs.base_path.OpenFile(ctx, name, flag, perm)
 }
 
 func (fs *twdsFS) RemoveAll(ctx context.Context, name string) error {
-	real_file_name := filepath.Join(fs.base_path, name)
-	return os.RemoveAll(real_file_name)
+	return fs.base_path.RemoveAll(ctx, name)
 }
 
 func (fs *twdsFS) Rename(ctx context.Context, old_name, new_name string) error {
-	real_old_name := filepath.Join(fs.base_path, old_name)
-	real_new_name := filepath.Join(fs.base_path, new_name)
-	return os.Rename(real_old_name, real_new_name)
+	return fs.base_path.Rename(ctx, old_name, new_name)
 }
 
 func (fs *twdsFS) Stat(ctx context.Context, name string) (os.FileInfo, error) {
-	real_file_name := filepath.Join(fs.base_path, name)
-	return os.Stat(real_file_name)
+	return fs.base_path.Stat(ctx, name)
 }
